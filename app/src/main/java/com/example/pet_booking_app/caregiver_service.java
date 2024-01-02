ad06726e6e9da1430eb97327577397ab8073fc53
mayurasandakalum <mssellapperuma@gmail.com>
2024-01-02 13:33:13 +0530

    add: files for caregiver service

---
+++ app/src/main/java/com/example/pet_booking_app/caregiver_service.java
@@ -0,0 +1,14 @@
+package com.example.pet_booking_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class caregiver_service extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_caregiver_service);
+    }
+}
