32a79aaddbc9bc8e081f6289c0a4daf7d6a83235
mayurasandakalum <mssellapperuma@gmail.com>
2024-01-07 13:42:03 +0530

    add: Customer class

---
+++ app/src/main/java/com/example/pet_booking_app/models/Customer.java
@@ -0,0 +1,13 @@
+package com.example.pet_booking_app.models;
+
+public class Customer {
+
+    private int id;
+    private String full_name;
+    private String address;
+    private String birthday;
+    private String gender;
+    private String phone_number;
+    private String email;
+    private String password;
+}
