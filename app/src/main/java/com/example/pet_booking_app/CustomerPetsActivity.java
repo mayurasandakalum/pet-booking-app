30f666e8231bac118b748b87d80dcfce7b207db4
mayurasandakalum <mssellapperuma@gmail.com>
2024-01-08 16:13:20 +0530

    add: CustomerPetsActivity

---
+++ app/src/main/java/com/example/pet_booking_app/CustomerPetsActivity.java
@@ -0,0 +1,52 @@
+package com.example.pet_booking_app;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.text.Html;
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.pet_booking_app.databinding.ActivityCustomerPetsBinding;
+
+public class CustomerPetsActivity extends AppCompatActivity {
+
+    private AppBarConfiguration appBarConfiguration;
+    private ActivityCustomerPetsBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityCustomerPetsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.toolbar);
+        getSupportActionBar().setTitle(Html
+                .fromHtml("<font color='#ffffff'> Your pets </font>"));
+        getWindow().setStatusBarColor(getResources().getColor(R.color.text_primary)); // Change the color here
+
+        binding.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAnchorView(R.id.fab)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+//    @Override
+//    public boolean onSupportNavigateUp() {
+////        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_customer_pets);
+////        return NavigationUI.navigateUp(navController, appBarConfiguration)
+////                || super.onSupportNavigateUp();
+//    }
+}
